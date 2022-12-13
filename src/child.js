89a469c9eab3ff5e315587e5c5be0b12c72d167a
Muskan Valmikee <muskanvalmikee@gmail.com>
2022-12-13 16:06:11 +0530

    here we added css.

--- src/child.js
+++ src/child.js
@@ -1,11 +1,13 @@
+import './App.css'
 export default function Child(props){
   return(
-      <div>
-      <h1>this is {props.count}</h1>
-      <button onClick={()=>{
-          props.setCount(200)
-      }}>click</button>
-      <h1>{props.arr}</h1>
+      <div id="box">
+        <h1>How we can use Props.</h1>
+        <h2>this is {props.count}</h2>
+        <button onClick={()=>{
+            props.setCount(200)
+        }}>click</button>
+        <h2>{props.arr}</h2>
       </div>
   )
 }
